d88d4d8385663eb5893187e1b4cde1584413917c
skempfer <shana.kempfer88@gmail.com>
2026-03-29 22:15:11 -0300

    Add exercise 8.5

--- src/resolvers.js
+++ src/resolvers.js
@@ -5,11 +5,17 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books
+      let filteredBooks = books
+
+      if (args.author) {
+        filteredBooks = filteredBooks.filter((book) => book.author === args.author)
       }
 
-      return books.filter((book) => book.author === args.author)
+      if (args.genre) {
+        filteredBooks = filteredBooks.filter((book) => book.genres.includes(args.genre))
+      }
+
+      return filteredBooks
     },
     allAuthors: () => authors,
   },
